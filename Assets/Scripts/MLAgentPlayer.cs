7d9699cdc3fe554dde775117e8187237d38e2228
AntonioCimino <45990657+AntonioCimino@users.noreply.github.com>
2021-11-05 15:57:12 +0100

    initializes project

---
+++ Assets/Scripts/MLAgentPlayer.cs
@@ -0,0 +1,115 @@
+﻿using Unity.MLAgents;
+using UnityEngine;
+
+public class MLAgentPlayer : Agent
+{
+    public float force = 50.0f;
+    public Transform reset = null;
+    public TextMesh score = null;
+    public Vector3 jump;
+    public Vector3 right;
+    public Vector3 left;
+    private Rigidbody rb = null;
+    public bool air = true;
+    private float points = 0;
+    public float moveSpeed = 0.5f;
+    
+    public override void Initialize()
+    {
+        rb = this.GetComponent<Rigidbody>();
+        jump = new Vector3(0.0f, 0.15f, 0.0f);
+        right = new Vector3(0.15f, 0.0f, 0.0f);
+        left = new Vector3(-0.15f, 0.0f, 0.0f);
+        ResetMyAgent();
+    }
+
+    public override void OnActionReceived(float[] vectorAction)
+    {
+        if (vectorAction[0] == 3)
+        {
+            UpForce();
+        }
+
+        if (vectorAction[0] == 1)
+        {
+            MoveRight();
+        }
+
+        if (vectorAction[0] == 2)
+        {
+            MoveLeft();
+        }
+    }
+
+    public override void OnEpisodeBegin()
+    {
+        ResetMyAgent();
+    }
+
+    public override void Heuristic(float[] actionsOut)
+    {
+        actionsOut[0] = 0;
+        //actionsOut[1] = 0;
+        if (Input.GetKey(KeyCode.UpArrow) == true)
+            actionsOut[0] = 3;
+        if (Input.GetKey(KeyCode.RightArrow) == true)
+            actionsOut[0] = 1;
+        if (Input.GetKey(KeyCode.LeftArrow) == true)
+            actionsOut[0] = 2;
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("obstacle") == true || collision.gameObject.CompareTag("obstacle1") == true
+        || collision.gameObject.CompareTag("obstacle2") == true)
+        {
+            AddReward(-1.0f);          
+            Destroy(collision.gameObject);
+            EndEpisode();
+        }
+
+        if (collision.gameObject.CompareTag("walltop") == true || collision.gameObject.CompareTag("floor") == true)
+        {
+            AddReward(-0.9f);
+            EndEpisode();
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("wallward") == true || other.CompareTag("wallward1") == true || other.CompareTag("wallward2") == true)
+        {
+            AddReward(0.1f);
+            points++;
+            score.text = "Score: " + points.ToString();
+        }     
+    }
+
+    private void UpForce()
+    {
+        if(this.transform.position.y < 2f){
+            air = true;
+        }
+
+        if(air == true){
+            rb.AddForce(jump * force, ForceMode.Impulse);
+            air = false;
+        }
+    }
+
+    private void MoveRight()
+    {
+        rb.AddForce(right * moveSpeed, ForceMode.Impulse);
+    }
+
+    private void MoveLeft()
+    {
+        rb.AddForce(left * moveSpeed, ForceMode.Impulse);
+    }
+
+    private void ResetMyAgent()
+    {
+        this.transform.position = new Vector3(reset.position.x, reset.position.y, reset.position.z);
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject prefab = null;
+    public GameObject prefab1 = null;
+    public GameObject prefab2 = null;
+
+    private void Start()
+    {
+        Invoke("SpawnObstacle", 1.0f);
+    }
+    private void SpawnObstacle()
+    {
+        int n = Random.Range(0, 3);
+
+        if(n == 0){
+            GameObject go = Instantiate(prefab);
+            go.transform.position = new Vector3(-8.60f, 1.50f, -7.50f);
+            Invoke("SpawnObstacle", 5.0f);
+        }
+
+         if(n == 1){
+            GameObject go = Instantiate(prefab1);
+            go.transform.position = new Vector3(-10.60f, 7.50f, -1f);
+            Invoke("SpawnObstacle", 5.0f);
+         }
+
+         if(n == 2){
+            GameObject go = Instantiate(prefab2);
+            go.transform.position = new Vector3(-6.60f, 7.50f, 1f);
+            Invoke("SpawnObstacle", 5.0f);
+         }
+    }
+}
